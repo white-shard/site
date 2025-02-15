72f4a2fb549fb6c874f2ee3ea1e4acb7391900d9
ClausCode <nclaus.code@gmail.com>
2025-02-15 23:52:32 +0300

    minor: fix styles

---
+++ src/app/(application)/cases/[id]/page.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image"
+import { notFound } from "next/navigation"
+
+import { Media } from "@/dashboard/payload-types"
+
+import { getCaseById } from "@/shared/lib/api"
+
+export default async function CasePage({
+	params
+}: {
+	params: Promise<{ id: string }>
+}) {
+	const { id } = await params
+	try {
+		const data = await getCaseById(Number(id))
+		if (!data) return notFound()
+
+		return (
+			<div className="container mx-auto flex max-w-screen-xl flex-col gap-4 p-4">
+				<h2 className="from-primary to-secondary bg-gradient-to-r bg-clip-text text-4xl font-bold text-transparent">
+					{data.name}
+				</h2>
+				<p className="text-secondary max-w-xl">{data.description}</p>
+				{data.pictures?.map((it) => {
+					const picture = it.picture as Media
+					return (
+						<Image
+							key={it.id}
+							src={picture?.url || "#"}
+							alt={picture?.alt || "#"}
+							width={1920}
+							height={1080}
+						/>
+					)
+				})}
+			</div>
+		)
+	} catch {
+		return notFound()
+	}
+}
---
+++ src/shared/lib/api.ts
@@ -0,0 +1,22 @@
+"use server"
+
+import { Case } from "@/dashboard/payload-types"
+
+import { privateConfig } from "@/shared/config/private"
+
+export async function getCaseById(id: number) {
+	const response = await fetch(
+		`${privateConfig.NEXT_PUBLIC_API_URL}/cases/${id}`,
+		{
+			next: {
+				revalidate: Number(privateConfig.REVALIDATE_RARE)
+			}
+		}
+	)
+
+	if (!response.ok) return undefined
+	const data = (await response.json()) as Case
+
+	if (!data) return undefined
+	return data
+}
