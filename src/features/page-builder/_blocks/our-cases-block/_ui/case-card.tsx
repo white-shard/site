9ca00a0030029b57dfa164a39f78a378de274e52
ClausCode <nclaus.code@gmail.com>
2025-02-15 23:09:19 +0300

    refactor: update CaseCard component with improved design and interaction

--- src/features/page-builder/_blocks/our-cases-block/_ui/case-card.tsx
+++ src/features/page-builder/_blocks/our-cases-block/_ui/case-card.tsx
@@ -1,4 +1,4 @@
-import { ArrowRight } from "lucide-react"
+import { ExternalLink } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -9,30 +9,31 @@ type Props = {
 }
 export function CaseCard({ data }: Props) {
 	return (
-		<div className="group relative">
-			<div className="relative overflow-hidden rounded-xl shadow-lg">
-				<Image
-					src={data.pictures[0].picture.url || "/placeholder.svg"}
-					alt={data.name}
-					width={600}
-					height={400}
-					className="h-64 w-full object-cover transition-transform duration-300 group-hover:scale-110"
-				/>
-				<div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent" />
-				<div className="absolute bottom-0 left-0 p-6">
-					<span className="mb-2 inline-block rounded-full bg-purple-600 px-3 py-1 text-xs font-semibold tracking-wider text-white">
-						{data.service}
-					</span>
-					<h3 className="mb-2 text-2xl font-bold text-white">{data.name}</h3>
-					<p className="mb-4 text-gray-300">{data.description}</p>
-					<Link
-						href={`/cases/${data.id}`}
-						className="inline-flex items-center text-purple-400 transition duration-300 hover:text-purple-300"
-					>
-						Узнать больше <ArrowRight className="ml-2 h-4 w-4" />
-					</Link>
+		<Link href={`/cases/${data.id}`}>
+			<div className="group relative">
+				<div className="relative overflow-hidden rounded-xl shadow-lg">
+					<Image
+						src={data.pictures[0].picture.url || "/placeholder.svg"}
+						alt={data.name}
+						width={600}
+						height={400}
+						className="h-64 w-full object-cover transition-transform duration-300 group-hover:scale-110"
+					/>
+					<div className="absolute inset-0 bg-gradient-to-t from-black/80 to-transparent" />
+					<div className="absolute bottom-0 left-0 p-4">
+						<span className="bg-primary text-primary-foreground mb-2 inline-block rounded-full px-3 py-1 text-xs font-semibold tracking-wider">
+							{data.service}
+						</span>
+						<h3 className="text-primary-foreground mb-2 text-2xl font-bold">
+							{data.name}
+						</h3>
+						<p className="line-clamp-2 text-gray-400">{data.description}</p>
+					</div>
+				</div>
+				<div className="bg-primary text-primary-foreground absolute -top-2 -right-2 rounded-full p-2 opacity-0 shadow-lg transition-opacity duration-300 group-hover:opacity-100">
+					<ExternalLink className="size-5" />
 				</div>
 			</div>
-		</div>
+		</Link>
 	)
 }
