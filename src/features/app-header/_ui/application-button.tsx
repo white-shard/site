cf0b985c05eb9c8e51be70c39e2a3749ed75abd1
ClausCode <nclaus.code@gmail.com>
2025-02-21 21:17:01 +0300

    refactor: update application button with new icon and link behavior

--- src/features/app-header/_ui/application-button.tsx
+++ src/features/app-header/_ui/application-button.tsx
@@ -1,12 +1,18 @@
+import Link from "next/link"
+
 import { Button } from "@/shared/ui/button"
 
-import { PhoneApplicationIcon } from "@/shared/icons/phone-application.icon"
+import { AddDiamondIcon } from "@/shared/icons/add-diamond.icon"
 
 export function ApplicationButton() {
 	return (
 		<>
-			<Button className="hidden md:block">Оставить заявку</Button>
-			<PhoneApplicationIcon className="text-primary size-6 md:hidden" />
+			<Button asChild className="hidden md:block">
+				<Link href="#offer">Оставить заявку</Link>
+			</Button>
+			<Link href="#offer" className="md:hidden">
+				<AddDiamondIcon className="text-primary size-8 animate-spin duration-5000" />
+			</Link>
 		</>
 	)
 }
